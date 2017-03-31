ada98cdb5a67ad72656e0b444f5d5407efb0df57
HackeZ <767110505@qq.com>
2017-03-31 16:30:04 +0800

    all controller finish

--- GutHub/srv/controller/gut.go
+++ GutHub/srv/controller/gut.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 	"log"
 	"sync"
 
@@ -14,15 +12,15 @@ import (
 var (
 	// GutList save all guts
 	gutList []*protocol.Gut
-	// gutID identity of gut
-	gutID int64
-	// Mutex for make sure ID unqueness
+	// commonID identity of gut
+	commonID int
+	// Mutex for make sure ID uniqueness
 	mutex *sync.Mutex
 )
 
 func init() {
 	gutList = make([]*protocol.Gut, 0, 256)
-	id = len(gutList)
+	commonID = len(gutList) + 1
 	mutex = &sync.Mutex{}
 }
 
@@ -32,10 +30,9 @@ func GetGutList(ctx *fasthp.RequestCtx) {
 	returnData.Code = -1
 	log.Println("got GetGutList req.")
 
-	var data bytes.Buffer
 	result, err := json.Marshal(gutList)
 	if err != nil {
-		returnData.Message = "get gut list failed, error:" + err.Error
+		returnData.Message = "get gut list failed, error:" + err.Error()
 		log.Fatalln(returnData.Message)
 		result, _ = json.Marshal(returnData)
 
@@ -60,7 +57,7 @@ func AddGut(ctx *fasthp.RequestCtx) {
 	returnData.Code = -1
 	log.Println("got AddGut req.")
 
-	if !ctx.IsPost {
+	if !ctx.IsPost() {
 		returnData.Message = "can not handle AddGut do not in POST method."
 		log.Fatalln(returnData.Message)
 		result, _ := json.Marshal(returnData)
@@ -84,8 +81,8 @@ func AddGut(ctx *fasthp.RequestCtx) {
 	}
 
 	mutex.Lock()
-	id := gutID
-	gutID++
+	id := commonID
+	commonID++
 	mutex.Unlock()
 
 	gutList = append(gutList, func() *protocol.Gut {
@@ -96,7 +93,7 @@ func AddGut(ctx *fasthp.RequestCtx) {
 	returnData.Code = 0
 	returnData.Message = "add gut to list success"
 	log.Println(returnData.Message)
-	result, _ = json.Marshal(returnData)
+	result, _ := json.Marshal(returnData)
 
 	ctx.SetContentType("application/json")
 	ctx.SetStatusCode(200)
@@ -105,9 +102,93 @@ func AddGut(ctx *fasthp.RequestCtx) {
 }
 
 func ModifyGut(ctx *fasthp.RequestCtx) {
+	var returnData protocol.ReturnData
+	returnData.Code = -1
 	log.Println("got ModifyGut req.")
+
+	gutByte := ctx.FormValue("gut")
+	var gutChange protocol.Gut
+	err := json.Unmarshal(gutByte, &gutChange)
+	if err != nil {
+		returnData.Message = "unmarshal data failed, error:" + err.Error()
+		log.Fatalln(returnData.Message)
+		result, _ := json.Marshal(returnData)
+
+		ctx.SetStatusCode(408)
+		ctx.Write(result)
+		return
+	}
+
+	for _, gut := range gutList {
+		if gut.ID == gutChange.ID {
+			gut = &gutChange
+			returnData.Code = 0
+			returnData.Message = "modify gut success"
+			log.Println(returnData.Message)
+			result, _ := json.Marshal(returnData)
+
+			ctx.SetStatusCode(200)
+			ctx.Write(result)
+			return
+		}
+	}
+
+	returnData.Message = "can not find this gut in list"
+	log.Fatalln(returnData.Message)
+	result, _ := json.Marshal(returnData)
+
+	ctx.SetStatusCode(408)
+	ctx.Write(result)
+	return
 }
 
 func DeleteGut(ctx *fasthp.RequestCtx) {
+	var returnData protocol.ReturnData
+	returnData.Code = -1
 	log.Println("got DeleteGut req.")
+
+	gutByte := ctx.FormValue("gut")
+	var id int
+	err := json.Unmarshal(gutByte, &id)
+	if err != nil {
+		returnData.Message = "unmarshal data failed, error:" + err.Error()
+		log.Fatalln(returnData.Message)
+		result, _ := json.Marshal(returnData)
+
+		ctx.SetStatusCode(408)
+		ctx.Write(result)
+		return
+	}
+
+	if id <= 0 {
+		returnData.Message = "validate id"
+		log.Fatalln(returnData.Message)
+		result, _ := json.Marshal(returnData)
+
+		ctx.SetStatusCode(408)
+		ctx.Write(result)
+		return
+	}
+
+	for index, gut := range gutList {
+		if gut.ID == id {
+			gutList = append(gutList[:index], gutList[index+1:]...)
+			returnData.Code = 0
+			returnData.Message = "delete gut success"
+			log.Println(returnData.Message)
+			result, _ := json.Marshal(returnData)
+
+			ctx.SetStatusCode(200)
+			ctx.Write(result)
+			return
+		}
+	}
+
+	returnData.Message = "can not find this gut in list"
+	log.Fatalln(returnData.Message)
+	result, _ := json.Marshal(returnData)
+
+	ctx.SetStatusCode(408)
+	ctx.Write(result)
+	return
 }
